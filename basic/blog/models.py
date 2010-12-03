5cbf8e1baa7eb4e2b2a80470f7c8a45c5a2b362c
Dana Woodman <dana@danawoodman.com>
2010-12-02 19:23:01 -0800

    Adding parent relationship to Blog Category model

--- basic/blog/models.py
+++ basic/blog/models.py
@@ -15,6 +15,7 @@ class Category(models.Model):
     """Category model."""
     title = models.CharField(_('title'), max_length=100)
     slug = models.SlugField(_('slug'), unique=True)
+    parent = models.ForeignKey('self', blank=True, null=True, related_name='child_set')
 
     class Meta:
         verbose_name = _('category')
